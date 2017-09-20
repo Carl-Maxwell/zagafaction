e7d8cf266a421d979a3219f798524d7c507eec89
Carl Maxwell <carl.maxwell@gmail.com>
2017-09-19 21:37:18 -0700

    centered & scaled the rasem output
    * increased size of rasem output to 800*800
    * should automagically scale and offset any reasonable node count to fit into output

--- lib/zagafaction.rb
+++ lib/zagafaction.rb
@@ -19,18 +19,46 @@ if $PROGRAM_NAME == __FILE__
   # end
 
   File.open("test.svg", "w") do |f|
-    Rasem::SVGImage.new({width: 100, height: 100}, f) do
+    Rasem::SVGImage.new({width: 800, height: 800}, f) do
 
       # TODO find an offset that keeps everything on the render
 
-      scale = 10.0
+      scale = 1.0 # 10.0
+
+      #
+      # find offsets for centering
+      #
+
+      top_left = Vector.new([0.0, 0.0])
+      bottom_right = Vector.new([0.0, 0.0])
+
+      x_sorted = nodes.sort {|a, b| a.x <=> b.x }
+      y_sorted = nodes.sort {|a, b| a.y <=> b.y }
+
+      top_left[0] = x_sorted.first.x
+      top_left[1] = y_sorted.first.y
+
+      bottom_right[0] = x_sorted.last.x
+      bottom_right[1] = y_sorted.last.y
+
+      scale *= [400/(bottom_right - top_left).length, 1.0].max
+
+      offset = Vector.new([0 - top_left.x, 0 - top_left.y])
+
+      offset *= scale
+      offset += [200, 200]
 
       nodes.each do |node|
-        circle(node.x*scale, node.y*scale, scale/2)
+        p1 = node.position * scale + offset
+
+        circle(p1.x, p1.y, scale/2)
 
         node.connections.each do |connection|
           other = connection.other
-          line(node.x*scale, node.y*scale, other.x*scale, other.y*scale)
+
+          p2 = other.position * scale + offset
+
+          line(p1.x, p1.y, p2.x, p2.y)
         end
       end
 
